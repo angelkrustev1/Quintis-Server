db9d675ff494db3346975e94ce0eed0124d97908
angelkrustev1 <angelkrustev07@gmail.com>
2025-05-01 19:48:53 +0300

    Feature: DRF

--- quintis_server/settings.py
+++ quintis_server/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = []
 LOCAl_APPS = [
     'accounts',
     'meetings',
+    'questions',
     'polls',
 ]
 
@@ -35,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ] + LOCAl_APPS
 
 MIDDLEWARE = [
